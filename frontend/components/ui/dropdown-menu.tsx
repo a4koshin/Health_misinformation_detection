1f70f3c6d9e2c0d957c5658fe4ecb665abc93ac7
a4koshin <mankajr11@gmail.com>
2026-07-08 22:22:29 +0300

    Replace dropdown menu Lucide icons with MaterialIcon.

--- frontend/components/ui/dropdown-menu.tsx
+++ frontend/components/ui/dropdown-menu.tsx
@@ -4,7 +4,7 @@ import * as React from "react"
 import { DropdownMenu as DropdownMenuPrimitive } from "radix-ui"
 
 import { cn } from "@/lib/utils"
-import { CheckIcon, ChevronRightIcon } from "lucide-react"
+import { MaterialIcon } from "@/components/ui/material-icon"
 
 function DropdownMenu({
   ...props
@@ -106,8 +106,7 @@ function DropdownMenuCheckboxItem({
         data-slot="dropdown-menu-checkbox-item-indicator"
       >
         <DropdownMenuPrimitive.ItemIndicator>
-          <CheckIcon
-          />
+          <MaterialIcon name="check" size={16} />
         </DropdownMenuPrimitive.ItemIndicator>
       </span>
       {children}
@@ -149,8 +148,7 @@ function DropdownMenuRadioItem({
         data-slot="dropdown-menu-radio-item-indicator"
       >
         <DropdownMenuPrimitive.ItemIndicator>
-          <CheckIcon
-          />
+          <MaterialIcon name="check" size={16} />
         </DropdownMenuPrimitive.ItemIndicator>
       </span>
       {children}
@@ -232,7 +230,7 @@ function DropdownMenuSubTrigger({
       {...props}
     >
       {children}
-      <ChevronRightIcon className="ml-auto" />
+      <MaterialIcon name="chevron_right" size={16} className="ml-auto" />
     </DropdownMenuPrimitive.SubTrigger>
   )
 }
