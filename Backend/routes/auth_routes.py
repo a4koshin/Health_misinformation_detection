20a30123fca8b14592abf320e080f374fc3215d8
a4koshin <mankajr11@gmail.com>
2026-08-04 00:19:09 +0300

    Update Flask auth routes.

--- Backend/routes/auth_routes.py
+++ Backend/routes/auth_routes.py
@@ -4,8 +4,10 @@ from controllers import auth_controller
 
 auth_bp = Blueprint("auth", __name__, url_prefix="/api/auth")
 
-auth_bp.add_url_rule("/register", view_func=auth_controller.register, methods=["POST"])
-auth_bp.add_url_rule("/login", view_func=auth_controller.login, methods=["POST"])
+auth_bp.add_url_rule(
+    "/register", view_func=auth_controller.register, methods=["POST"])
+auth_bp.add_url_rule(
+    "/login", view_func=auth_controller.login, methods=["POST"])
 auth_bp.add_url_rule("/me", view_func=auth_controller.me, methods=["GET"])
 auth_bp.add_url_rule(
     "/me",
