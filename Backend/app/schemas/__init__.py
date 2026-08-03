0330183fe809e91f79ca53d0d4b6a42882569396
a4koshin <mankajr11@gmail.com>
2026-08-04 00:18:44 +0300

    Remove Backend/app/schemas/__init__.py.

--- Backend/app/schemas/__init__.py
+++
@@ -1,4 +0,0 @@
-from app.schemas.auth import Token, UserRegister, UserResponse
-from app.schemas.detection import DetectionResponse
-
-__all__ = ["UserRegister", "UserResponse", "Token", "DetectionResponse"]
