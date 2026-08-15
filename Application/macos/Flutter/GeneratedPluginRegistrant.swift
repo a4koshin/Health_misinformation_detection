8940ee76ef6ce6433e5baf2c927b54fddb8f5b56
a4koshin <mankajr11@gmail.com>
2026-08-15 17:39:19 +0300

    Register Flutter plugins for macOS notifications.

--- Application/macos/Flutter/GeneratedPluginRegistrant.swift
+++ Application/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,11 +6,13 @@ import FlutterMacOS
 import Foundation
 
 import file_picker
+import flutter_local_notifications
 import flutter_secure_storage_darwin
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   FlutterSecureStorageDarwinPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageDarwinPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
