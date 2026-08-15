045f83211349abd4ec499eff1805e9108caf0ebb
a4koshin <mankajr11@gmail.com>
2026-08-15 17:39:19 +0300

    Improve Flutter API error message parsing.

--- Application/lib/app/core/network/api_exception.dart
+++ Application/lib/app/core/network/api_exception.dart
@@ -1,8 +1,13 @@
 class ApiException implements Exception {
-  const ApiException(this.message, {this.status = 0});
+  const ApiException(
+    this.message, {
+    this.status = 0,
+    this.paymentOutcome,
+  });
 
   final String message;
   final int status;
+  final String? paymentOutcome;
 
   @override
   String toString() => message;
